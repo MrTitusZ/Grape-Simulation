0032af798c865449c2145f2a2ef19d6de89335d0
Novák Csaba <csaba.novak@bayzoltan.hu>
2021-08-06 13:10:46 +0200

    Finalizing

--- Generate Objects/Assets/Scripts/ObjectSpawner.cs
+++ Generate Objects/Assets/Scripts/ObjectSpawner.cs
@@ -4,43 +4,89 @@ using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
 {
-    [Header("Tõke Beállítások")]
     [SerializeField] GameObject[] tokek;
-    [SerializeField] int tokekSzama = 0;
-    [SerializeField] int sorokSzama = 1;
+    [SerializeField] GameObject oszlop;
 
-    [Header("")]
-    [SerializeField] float sorokKozottiFixTavolsag = 2f;
-    [SerializeField] float sorokKozottiRandomTavolsag = 0.5f;
+    [Header("Tõke Beállítások")]
+    [SerializeField] int tokekSzama = 10;
     [SerializeField] float tokekKozottiTavolsag = 2f;
     [SerializeField] float tokekKozottiRandomTavolsag = 0.5f;
-    [SerializeField] float tokeRandomYRotationFokban = 30f;
+    [SerializeField] float tokeRandomRotationFokban = 10f;
+    [SerializeField] int tokeSorokSzama = 5;
+    [SerializeField] float tokeSorokKozottiTavolsag = 2f;
+    
+    [Header("Oszlop Beállítások")]
+    [SerializeField] int oszlopokSzama = 5;
+    [SerializeField] float oszlopokKözöttiTavolsag = 6f;
+    [SerializeField] int oszlopSorokSzama = 5;
+    [SerializeField] float oszlopSorokKozottiTavolsag = 2;
 
-    float zCord = 1;
-    float tokeRandomTavolsag;
-    int tokeRandomIndex;
-    float tokeRandomRotation;
+    [Header("Drótmezõ Beállítások")]
+    [SerializeField] Color lineColor = new Color(1, 1, 1, 255);
+
+    // Listában eltárolom a futáskor példányosított oszlopokat GameObject-ként
+    List<GameObject> oszlopok = new List<GameObject>();
+    int randomTokeModell;
+    float randomTokeTavolsag;
+    float randomTokeRotation;
+    float xPos;
+    float yPos;
+    float zPos;
 
     void Start()
     {
         GenerateGrapeField();
     }
 
-    private void GenerateGrapeField()
+    void GenerateGrapeField()
     {
-        for (int j = 0; j < sorokSzama; j++)
+        // Tõkék példányosítása
+        for (int sorCounter = 0; sorCounter < tokeSorokSzama; sorCounter++)
         {
-            for (int i = 0; i < tokekSzama; i++)
+            for (int tokeCounter = 0; tokeCounter < tokekSzama; tokeCounter++)
             {
-                tokeRandomIndex = Random.Range(0, tokek.Length);
-                tokeRandomRotation = Random.Range(-tokeRandomYRotationFokban, tokeRandomYRotationFokban);
-                sorokKozottiRandomTavolsag = Random.Range(0, sorokKozottiRandomTavolsag);
-                Vector3 grapePosition = new Vector3(1 + j * sorokKozottiFixTavolsag, 0f, zCord);
-                Instantiate(tokek[tokeRandomIndex], grapePosition, Quaternion.Euler(0f, tokeRandomRotation, 0f));
-                tokeRandomTavolsag = Random.Range(0, tokekKozottiRandomTavolsag);
-                zCord += tokekKozottiTavolsag + tokeRandomTavolsag;
+                randomTokeModell = Random.Range(0, tokek.Length);
+                randomTokeTavolsag = Random.Range(-tokekKozottiRandomTavolsag, tokekKozottiRandomTavolsag);
+                randomTokeRotation = Random.Range(-tokeRandomRotationFokban, tokeRandomRotationFokban);
+                xPos = transform.position.x + sorCounter * tokeSorokKozottiTavolsag;
+                zPos = transform.position.z + 0.5f + tokeCounter * tokekKozottiTavolsag + randomTokeTavolsag;
+                yPos = Terrain.activeTerrain.SampleHeight(new Vector3(xPos, 0, zPos));
+                Vector3 tokePosition = new Vector3(xPos, yPos, zPos);
+                Instantiate(tokek[randomTokeModell], tokePosition, Quaternion.Euler(0, randomTokeRotation, 0));
             }
-            zCord = 1;
+        }
+
+        // Oszlopok példányosítása
+        for (int sorCounter = 0; sorCounter < oszlopSorokSzama; sorCounter++)
+        {
+            for (int oszlopCounter = 0; oszlopCounter < oszlopokSzama; oszlopCounter++)
+            {
+                xPos = transform.position.x + sorCounter * oszlopSorokKozottiTavolsag;
+                zPos = transform.position.z + oszlopCounter * oszlopokKözöttiTavolsag;
+                yPos = Terrain.activeTerrain.SampleHeight(new Vector3(xPos, 0, zPos));
+                Vector3 oszlopPosition = new Vector3(xPos, yPos, zPos);
+                oszlopok.Add(Instantiate(oszlop, oszlopPosition, Quaternion.Euler(-90, 0, 0)) as GameObject);
+            }
+        }
+
+        // Drótmezõ kialakítása a példányosított oszlopokhoz
+        for(int sorCounter = 0; sorCounter < oszlopSorokSzama; sorCounter++)
+        {
+            for(int pointCounter = 0; pointCounter < 3; pointCounter++)
+            {
+                GameObject lineRendererGameObject = new GameObject();
+                lineRendererGameObject.AddComponent<LineRenderer>();
+                LineRenderer lineRenderer = lineRendererGameObject.GetComponent<LineRenderer>();
+                lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+                lineRenderer.widthMultiplier = 0.01f;
+                lineRenderer.positionCount = oszlopokSzama;
+                lineRenderer.startColor = lineColor;
+                lineRenderer.endColor = lineColor;
+                for(int oszlopCounter = 0; oszlopCounter < oszlopokSzama; oszlopCounter++)
+                {
+                    lineRenderer.SetPosition(oszlopCounter, oszlopok[sorCounter * oszlopokSzama + oszlopCounter].transform.GetChild(pointCounter).position);
+                }
+            }
         }
     }
 }
